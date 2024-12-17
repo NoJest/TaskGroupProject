eed00ce038c09c02a66194ebb356f7220300910f
Joyce Sheng <joycesheng96@hotmail.com>
2024-12-17 15:23:27 -0500

    added progressmodel

--- server/models.py
+++ server/models.py
@@ -3,4 +3,10 @@ from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
 
-# Models go here!
+class ProgressUpdate(db.Model, SerializerMixin):
+    __tablename__ = 'progress_update'
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    metric_value = db.Column(db.String, nullable=False)
+    notes = db.Column(db.text)
