6068b919985eb616d6363a22fb76cd8913661c0c
JustinpThomasson <Foreveraendeavour@gmail.com>
2024-12-17 15:12:31 -0500

    preference model

--- server/models.py
+++ server/models.py
@@ -1,6 +1,20 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
-
+from sqlalchemy.orm import validates
 from config import db
 
-# Models go here!
+class Preferences(db.Model, SerializerMixin):
+    __tablename__ = "profile_table"
+    
+    id = db.Column(db.Integer, primary_key= True)
+    commitment_time = db.Column(db.String, nullable = False)
+    career_path = db.Column(db.Integer)
+    notification_type = db.Column (db.String(40))
+    # mood = db.Column(db.String)
+    # Stretch goal
+    
+    # Relationships here
+    # users = db.relationship('User', back_populates = "preferences")
+    
+    # Serializing rules here
+    # serialize_rules = ('','', '', '')
