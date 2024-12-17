727dcc24b3728845bcbfcff516967399da5f3d6f
Sebastien <fenelonsebastien@outlook.com>
2024-12-17 15:23:21 -0500

    user table- Models.py

--- server/models.py
+++ server/models.py
@@ -3,4 +3,13 @@ from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
 
+
+class User(db.Model, SerializerMixin):
+
+    __tablename__= 'users_table'
+
+    id = db.Column(db.Integer, primary_key = True )
+    email = db.Column(db.String, unique=True, nullable=False)
+    phone = db.Column(db.Integer unique=True nullable=False)
+    password_hash = db.Colunm(db.string)
 # Models go here!
