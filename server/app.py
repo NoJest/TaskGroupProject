a6f40b18ca5302840cde8ecc305313f2b85967c9
JustinpThomasson <Foreveraendeavour@gmail.com>
2024-12-18 13:54:36 -0500

    finished crud for preferences

--- server/app.py
+++ server/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from flask import request, session
+from flask import request, session, jsonify
 from flask_restful import Resource
 from config import app, db, api
 from models import db, User, Goal, Preference, ProgressUpdate
@@ -8,25 +8,97 @@ from models import db, User, Goal, Preference, ProgressUpdate
 def find_user_by_id(user_id):
     return User.query.where(User.id == user_id).first()
 def find_preferences_by_id(user_id):
-    return Preference.query.where(Preference.user_id == user_id).first()
+    return Preference.query.where(Preference.user_id == user_id).all()
 
 
 # Preferences
-@app.get("/preferences/<int:user_id>")
+#get request
+@app.get("/api/preferences/<int:user_id>")
 def get_preferences_by_id(user_id):
     #1 find user with that id
     found_user = find_user_by_id(user_id)
-    
-       #too troubleshoot for none ids 
+    #if user exists return there preferences
     if found_user:
+        preferences = find_preferences_by_id(user_id)
         
-    #2 send it to the (client)
-        return found_user.preferences.to_dict(), 200
-    
+        #if preferences exist return them as a list of dictionaries
+        if preferences:
+            #probably wont jsonify, probably will do plaint text but leaving this for now
+            return jsonify([p.todict() for p in preferences]), 200
+        else:
+            return jsonify({"status": 404, "message": "No preferences found for this user" }), 404
     else:
-        #3 send a 404 if it doesnt exist
-        return {"status": 404, "message": "NOT FOUND" }, 404
+        return "User not found", 404
+#post request 
+@app.post('/api/preferences/<int:user_id>')
+def create_new_preferences():
+    data = request.json
+    try: 
+        new_preferences = Preference(
+            commitment_time= data.get('commitment_time'),
+            career_path = data.get('career_path'),
+            avatar = data.get('avatar'),
+            user_id = data.get('user_id')
+        )
+        
+        db.session.add(new_preferences)
+        db.session.commit()
+        return new_preferences.to_dict(), 201
+    except ValueError as validation_error:
+        return {
+            "status":400,
+            "message": "Invalid data entry",
+            "error_text": str(validation_error)
+        }, 400
+    except Exception as error: 
+        return {
+            "status": 400,
+            "message" : "something went really wrong, why did you set your preferences like that",
+            "error_text": str(error)
+            }, 400
 
+#delete request 
+@app.delete('/api/preferences/<int:user_id>')
+def delete_preferences_by_id(user_id):
+    found_preferences = find_user_by_id(user_id)
+    if found_preferences:
+        db.session.delete(found_preferences)
+        db.session.commit()
+        return {}, 204
+    #204 stands for no content, so empty message
+    else:
+        return {"status": 404, "message": "NOT FOUND" }, 404
+    
+#patch request
+@app.patch('/api/preferences/<int:user_id>')
+def edit_preferences(user_id):
+    found_user= find_user_by_id(user_id)
+    
+    if found_user:
+        #find preferences for this user
+        preferences = find_preferences_by_id(user_id)
+        
+        if preferences:
+            try:
+                data = request.json
+                for found_preference in preferences:
+                    for key, value in data.items():
+                        if hasattr(found_preference,key):
+                            setattr(found_preference, key, value)
+                db.session.commit()
+                updated_preferences = [pref.to_dict() for pref in preferences]
+                return jsonify(updated_preferences),202
+        
+            except Exception as e:
+                return { "status": 400, 
+                        "message": "Something is amiss updating preferences",
+                        "error_text": str(e)}, 400
+        else: 
+            return { "status": 404, 
+                        "message": "No preferences for this user"}, 400
+    else:
+        return {"status": 404, "message": "User not found"}, 404
+    
 
 
 #authentication and user login
@@ -35,6 +107,7 @@ def create_user():
     data = request.json
     try:
         new_user = User(email=data['email'])
+        new_user.phone = data ['phone']
         new_user.password = data['password']
         db.session.add(new_user)
         db.session.commit()
